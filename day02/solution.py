f910868722e144d87e4d4a46b6514c71c9dda415
Alex Johns <alexanderj393@yahoo.com>
2024-12-02 14:44:25 +0000

    Day 2 part 2

--- day02/solution.py
+++ day02/solution.py
@@ -6,14 +6,26 @@ with open("input.txt") as f:
         reports.append(report)
 
 # Part 1
-safe_count = 0
-
-for report in reports:
+def is_safe(report):
     differences = [int(b) - int(a) for a, b in zip(report, report[1:])]
-    
-    if all(1 <= diff <= 3 for diff in differences):
-        safe_count += 1
-    elif all(-3 <= diff <= -1 for diff in differences):
-        safe_count += 1
+    return all(1 <= diff <= 3 for diff in differences) or all(-3 <= diff <= -1 for diff in differences)
+
+safe_count = 0
+for report in reports:
+    safe_count += is_safe(report)
 
 print(f"Safe count: {safe_count}")
+
+# Part 2
+safe_count = 0
+for report in reports:
+    if is_safe(report):
+        safe_count += 1
+    else:
+        for i in range(len(report)):
+            new_report = report[:i] + report[i+1:]
+            if is_safe(new_report):
+                safe_count += 1
+                break
+
+print(f"Modified safe count: {safe_count}")
