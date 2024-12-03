52f5e6d2361f1247eb2dea9d89543f6b0f45b1eb
Alex Johns <alexanderj393@yahoo.com>
2024-12-03 12:47:06 +0000

    Day 3 part 2

--- day03/solution.py
+++ day03/solution.py
@@ -5,11 +5,33 @@ with open("input.txt") as f:
     lines = f.readlines()
 
 # Part 1
+def mul(mul_str):
+    a, b = map(int, re.findall(r"\d+", mul_str))
+    return a * b
+
 total = 0
+multiply_pattern = r"mul\(\d+,\d+\)"
 for line in lines:
-    matches = re.findall(r"mul\(\d+,\d+\)", line)
+    matches = re.findall(multiply_pattern, line)
     for match in matches:
-        a, b = map(int, re.findall(r"\d+", match))
-        total += a * b
+        total += mul(match)
 
 print(f"Multiplication total: {total}")
+
+# Part 2
+total = 0
+do = True
+do_pattern = r"do\(\)|don\'t\(\)"
+for line in lines:
+    parts = re.split(f"({do_pattern}|{multiply_pattern})", line)
+    for part in parts:
+        if part == "do()":
+            do = True
+        elif part == "don't()":
+            do = False
+        
+        match = re.match(multiply_pattern, part)
+        if match and do:
+            total += mul(match.group())
+
+print(f"Multiplication total with do/don't: {total}")
