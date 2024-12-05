1ebadcb866af9d5e4ac6b9b07914efc44d3ee9c0
Alex Johns <alexanderj393@yahoo.com>
2024-12-05 10:26:38 +0000

    Day 5 part 2

--- day05/solution.py
+++ day05/solution.py
@@ -13,21 +13,44 @@ with open("input.txt", "r") as f:
             updates.add(tuple(line.strip().split(',')))
 
 # Part 1
-middle_total = 0
-for update in updates:
-    ordered = True
+def check_ordered(update, rules):
     for i, page in enumerate(update):
-        if not ordered:
-            break
-
         check_pages = update[:i]
         if not set(check_pages).issubset(rules[page]):
-            ordered = False
-            break
-    
-    if ordered:
+            return False
+    return True
+
+def calc_middle(update):
+    middle_index = int(len(update) // 2)
+    return int(update[middle_index])
+
+middle_total = 0
+incorrect_updates = set()
+for update in updates:
+    if check_ordered(update, rules):
         update_list = list(update)
-        middle_index = int((len(update_list) - 1) / 2)
-        middle_total += int(update_list[middle_index])
+        middle_total += calc_middle(update_list)
+    else:
+        incorrect_updates.add(update)
 
 print(f"Total of middle elements: {middle_total}")
+
+# Part 2
+def order_update(update, rules):
+    ordered_update = list(update).copy()
+    while not check_ordered(ordered_update, rules):
+        for i in range(len(ordered_update)):
+            for key, pages in rules.items():
+                if key in ordered_update and ordered_update[i] in pages:
+                    key_index = ordered_update.index(key)
+                    page_index = ordered_update.index(ordered_update[i])
+                    if page_index > key_index:
+                        ordered_update.insert(key_index, ordered_update.pop(page_index))
+    return ordered_update
+
+middle_total = 0
+for update in incorrect_updates:
+    ordered_update = order_update(update, rules)
+    middle_total += calc_middle(ordered_update)
+
+print(f"Total of middle elements with updates ordered: {middle_total}")
