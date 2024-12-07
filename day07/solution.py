55a2543c44c0a030ad6f024f45fb5ee4a940478f
Alex Johns <alexanderj393@yahoo.com>
2024-12-07 12:00:42 +0000

    Day 7 part 2

--- day07/solution.py
+++ day07/solution.py
@@ -10,21 +10,32 @@ with open("input.txt", "r") as f:
         equations.append((result, params))
 
 # Part 1
-def try_equation(result, params):
+def try_equation(result, params, ops):
     if len(params) == 1:
         return result == params[0]
     
     a, b, *remaining = params
-    for op in [add, mul]:
-        if try_equation(result, [op(a, b)] + remaining):
+    for op in ops:
+        if try_equation(result, [op(a, b)] + remaining, ops):
             return True
     return False
-
 
 total = 0
 for equation in equations:
     result, params = equation
-    if try_equation(result, params):
+    if try_equation(result, params, [add, mul]):
         total += result
 
 print(f"Part 1: {total}")
+
+# Part 2
+def concat_int(a, b):
+    return int(str(a) + str(b))
+
+total = 0
+for equation in equations:
+    result, params = equation
+    if try_equation(result, params, [add, mul, concat_int]):
+        total += result
+
+print(f"Part 2: {total}")
