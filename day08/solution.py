3a2085fbeb0f89a4dc7559b607f5dd51ce70ae54
Alex Johns <alexanderj393@yahoo.com>
2024-12-08 12:57:43 +0000

    Day 8 part 1

---
+++ day08/solution.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+
+# Read input
+grid = dict()
+antennas = dict()
+with open("input.txt", "r") as f:
+    for i, line in enumerate(f):
+        for j, c in enumerate(line.strip()):
+            grid[(i, j)] = c
+
+            if c == ".":
+                continue
+                
+            if c not in antennas:
+                antennas[c] = []
+            antennas[c].append((i, j))
+
+# Part 1
+antinodes = set()
+
+for a, nodes in antennas.items():
+    for (i1, j1), (i2, j2) in permutations(nodes, 2):
+        di = (2 * i1) - i2
+        dj = (2 * j1) - j2
+        if (di, dj) in grid:
+            antinodes.add((di, dj))
+
+print(f"Part 1: {len(antinodes)}")
