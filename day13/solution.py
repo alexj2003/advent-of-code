1ef228734b9ae9c4aa832c6f749c88290fe8af32
Alex Johns <alexanderj393@yahoo.com>
2024-12-14 22:22:50 +0000

    Day 13 part 2

--- day13/solution.py
+++ day13/solution.py
@@ -1,3 +1,5 @@
+import sympy as sym
+
 # Read input
 machines = []
 with open("input.txt", "r") as f:
@@ -9,9 +11,12 @@ with open("input.txt", "r") as f:
         machine_desc["p"] = [int(x.split("=")[1]) for x in p.split(":")[1].split(", ")]
         machines.append(machine_desc)
 
+# Part 1
+
 MAX_PRESSES = 100
 
 # Solve a machine to find the minimum number of presses
+# Brute force approach
 def solve(machine):
     ax, ay = machine["a"]
     bx, by = machine["b"]
@@ -26,9 +31,35 @@ def solve(machine):
 
     return min(solutions) if solutions else 0
 
-# Part 1
+# total = 0
+# for machine in machines:
+#     total += solve(machine)
+
+# print(f"Part 1: {total}")
+
+# Part 2
+
+OFFSET = 10000000000000
+
+# For part 2, it can have an unlimited number of presses
+# A brute force approach is incredibly inefficient here
+# Solve as simultaneous equations instead
+def solve_v2(machine):
+    ax, ay = machine["a"]
+    bx, by = machine["b"]
+    px, py = machine["p"]
+    px += OFFSET
+    py += OFFSET
+
+    x, y = sym.symbols("x y", integer=True, positive=True)
+    x_eq = sym.Eq(ax * x + bx * y, px)
+    y_eq = sym.Eq(ay * x + by * y, py)
+    result = sym.solve([x_eq, y_eq], (x, y))
+
+    return 3 * result[x] + result[y] if result else 0
+
 total = 0
 for machine in machines:
-    total += solve(machine)
+    total += solve_v2(machine)
 
-print(f"Part 1: {total}")
+print(f"Part 2: {total}")
