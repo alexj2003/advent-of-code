369173c58e7a4dceff7ce5122a378c738db239b4
Alex Johns <alexanderj393@yahoo.com>
2024-12-16 14:17:11 +0000

    Day 16 part 2

--- day16/solution.py
+++ day16/solution.py
@@ -2,7 +2,6 @@ import heapq
 
 # Get start and end positions
 def get_start_end(maze):
-    #for pos, cell in maze.items():
     for y, row in enumerate(maze):
         for x, cell in enumerate(row):
             if cell == "S":
@@ -12,22 +11,26 @@ def get_start_end(maze):
     return start, end
 
 # Dijkstra's algorithm
-def dijkstra(maze):
+def dijkstra(maze, score_limit=None):
     start, end = get_start_end(maze)
     directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
-    queue = [(0, start, (1, 0))]
+    queue = [(0, start, (1, 0), {start})]
     visited = set()
 
     while len(queue) > 0:
-        d, (x, y), (dx, dy) = heapq.heappop(queue)
+        d, (x, y), (dx, dy), path = heapq.heappop(queue)
 
         if (x, y) in visited:
+            continue
+
+        # For part 2, skip paths that are longer than the shortest path
+        if score_limit is not None and d > score_limit:
             continue
 
         visited.add((x, y))
 
         if (x, y) == end:
-            return d
+            return d, path
         
         for i, j in directions:
             if i == -dx and j == -dy:
@@ -36,20 +39,19 @@ def dijkstra(maze):
             nx, ny = x + i, y + j
 
             is_straight = abs(i) == abs(dx) and abs(j) == abs(dy)
-            cost = 1
-            if not is_straight:
-                cost += 1000
+            cost = 1 if is_straight else 1001
 
             if maze[ny][nx] == "#" or (nx, ny) in visited:
                 continue
 
-            heapq.heappush(queue, (d + cost, (nx, ny), (i, j)))
+            new_path = path.copy()
+            new_path.add((nx, ny))
+            heapq.heappush(queue, (d + cost, (nx, ny), (i, j), new_path))
     
-    return None
+    return None, None
 
 def read_input(data):
     grid = data.split("\n")
-    # maze = {(i, j): cell for i, row in enumerate(grid) for j, cell in enumerate(row)}
     maze = [[cell for cell in row] for row in grid]
     return maze
 
@@ -78,5 +80,26 @@ with open("input.txt", "r") as f:
     maze = read_input(f.read())
 
 # Part 1
-lowest = dijkstra(maze)
+lowest, path = dijkstra(maze)
 print(f"Part 1: {lowest}")
+
+# Part 2
+tiles = set()
+tiles.update(path)
+
+# For each tile in path, replace with a wall and try to find a new path
+# Pretty slow but it works
+# I couldn't come up with a better way without completely rewriting part 1
+(sx, sy), (ex, ey) = get_start_end(maze)
+for x, y in path:
+    if (x, y) == (sx, sy) or (x, y) == (ex, ey):
+        continue
+
+    maze[y][x] = "#"
+    score, new_path = dijkstra(maze, lowest)
+    if score == lowest:
+        tiles.update(new_path)
+    
+    maze[y][x] = "."
+
+print(f"Part 2: {len(tiles)}")
