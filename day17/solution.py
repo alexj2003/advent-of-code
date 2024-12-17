f027fba9685b5d1efb671f982b8e95b187574713
Alex Johns <alexanderj393@yahoo.com>
2024-12-17 13:21:41 +0000

    Day 17 part 2

--- day17/solution.py
+++ day17/solution.py
@@ -71,8 +71,37 @@ with open("input.txt", "r") as f:
 
 # Program: 0,1,5,4,3,0"""
 
-registers, program = parse_input(data)
+# data = """Register A: 2024
+# Register B: 0
+# Register C: 0
+
+# Program: 0,3,5,4,3,0"""
 
 # Part 1
+registers, program = parse_input(data)
 output = read_instructions(registers, program)
 print(f"Part 1: {",".join(map(str, output))}")
+
+# Part 2
+a = 0
+for i in range(len(program)):
+    # Construct the program in reverse
+    search_for = program[len(program) - i - 1:]
+    offset = 0
+
+    while True:
+        # Shift a by 3 bits - program does the reverse when looping
+        a_val = (a << 3) + offset
+        new_regs = {
+            "A": a_val,
+            "B": 0,
+            "C": 0
+        }
+
+        output = read_instructions(new_regs, program)
+        if output == search_for:
+            a = a_val
+            break
+        offset += 1
+
+print(f"Part 2: {a}")
